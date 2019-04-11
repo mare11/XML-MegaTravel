e5bdf4e2400ee4a94712e45ca967b44545dfd0e1
mare11 <44523703+mare11@users.noreply.github.com>
2019-04-11 22:32:14 +0200

    Initial commit

---
+++ main-server/src/main/java/org/xmlws/megatravel/MegaTravelApplication.java
@@ -0,0 +1,13 @@
+package org.xmlws.megatravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegaTravelApplication.class, args);
+	}
+
+}
