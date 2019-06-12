688fb7e2ecc948ace0ad8af053493188b957e011
mare11 <44523703+mare11@users.noreply.github.com>
2019-06-12 15:00:00 +0200

    Update ReservationEndpoint.java

--- main-server/reservation-service/src/main/java/org/xmlws/reservationservice/endpoint/ReservationEndpoint.java
+++ main-server/reservation-service/src/main/java/org/xmlws/reservationservice/endpoint/ReservationEndpoint.java
@@ -5,20 +5,10 @@ import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
-import org.xmlws.dataservice.catalog.CatalogRepository;
 import org.xmlws.reservationservice.gen.GetAccReservationRequest;
 import org.xmlws.reservationservice.gen.GetReservationResponse;
 import org.xmlws.reservationservice.gen.GetUserReservationRequest;
-import org.xmlws.reservationservice.model.DirectionEnum;
-import org.xmlws.reservationservice.model.Message;
-import org.xmlws.reservationservice.model.Reservation;
-import org.xmlws.reservationservice.model.ReservationRating;
-import org.xmlws.reservationservice.repository.ReservationRepository;
 import org.xmlws.reservationservice.service.ReservationService;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Arrays;
 
 @Endpoint
 public class ReservationEndpoint {
@@ -26,27 +16,9 @@ public class ReservationEndpoint {
     @Autowired
     private ReservationService reservationService;
 
-    @Autowired
-    private CatalogRepository catalogRepository;
-
-    @Autowired
-    private ReservationRepository reservationRepository;
-
     @PayloadRoot(localPart = "getUserReservationRequest")
     @ResponsePayload
     public GetReservationResponse getReservationsByUser(@RequestPayload GetUserReservationRequest userReservationRequest) {
-
-
-//        dummy example
-//        ReservationRating reservationRating = new ReservationRating(2, "top comment", LocalDateTime.now(), true);
-//        Message message1 = new Message("ima li parkinga negdje blizu?!", LocalDateTime.now(), DirectionEnum.USER_TO_AGENT);
-//        Message message2 = new Message("NEMA NIGDJE!", LocalDateTime.now(), DirectionEnum.AGENT_TO_USER);
-//        Reservation reservation = new Reservation(2L, 1L,
-//                LocalDate.now(), LocalDate.now(), false, reservationRating, Arrays.asList(message1, message2));
-//        reservation.setId(catalogRepository.getCatalogId(reservationRepository.getRootElementName()));
-//
-//        reservationRepository.save(reservation);
-//
 
         GetReservationResponse response = new GetReservationResponse();
         reservationService.findReservationsByUser(userReservationRequest.getUserId())
