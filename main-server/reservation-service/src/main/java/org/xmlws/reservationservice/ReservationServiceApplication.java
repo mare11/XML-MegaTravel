73dd7a68a3750b113633401ac9e383b7a17fbb58
mare11 <44523703+mare11@users.noreply.github.com>
2019-06-14 15:49:48 +0200

    add microservice calls for reservations

--- main-server/reservation-service/src/main/java/org/xmlws/reservationservice/ReservationServiceApplication.java
+++ main-server/reservation-service/src/main/java/org/xmlws/reservationservice/ReservationServiceApplication.java
@@ -2,10 +2,21 @@ package org.xmlws.reservationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class ReservationServiceApplication {
+
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder getWebClientBuilder()
+    {
+        return WebClient.builder();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ReservationServiceApplication.class, args);
