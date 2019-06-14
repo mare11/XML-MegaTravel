da4f23c1a12a460a1b253c1c25c2babfc4127a0e
mare11 <44523703+mare11@users.noreply.github.com>
2019-06-14 23:03:59 +0200

    update reservation soap ws

--- main-server/reservation-service/src/main/java/org/xmlws/reservationservice/endpoint/ReservationEndpoint.java
+++ main-server/reservation-service/src/main/java/org/xmlws/reservationservice/endpoint/ReservationEndpoint.java
@@ -5,9 +5,8 @@ import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
-import org.xmlws.reservationservice.gen.GetAccReservationRequest;
+import org.xmlws.reservationservice.gen.GetReservationRequest;
 import org.xmlws.reservationservice.gen.GetReservationResponse;
-import org.xmlws.reservationservice.gen.GetUserReservationRequest;
 import org.xmlws.reservationservice.service.ReservationService;
 
 @Endpoint
@@ -16,20 +15,9 @@ public class ReservationEndpoint {
     @Autowired
     private ReservationService reservationService;
 
-    @PayloadRoot(localPart = "getUserReservationRequest")
+    @PayloadRoot(localPart = "getReservationRequest")
     @ResponsePayload
-    public GetReservationResponse getReservationsByUser(@RequestPayload GetUserReservationRequest userReservationRequest) {
-
-        GetReservationResponse response = new GetReservationResponse();
-        reservationService.findReservationsByUser(userReservationRequest.getUserId())
-                .stream().forEach(res -> response.getReservation().add(res));
-
-        return response;
-    }
-
-    @PayloadRoot(localPart = "getAccReservationRequest")
-    @ResponsePayload
-    public GetReservationResponse getReservationsByAccommodation(@RequestPayload GetAccReservationRequest accReservationRequest) {
+    public GetReservationResponse getReservationsByAccommodation(@RequestPayload GetReservationRequest accReservationRequest) {
 
         GetReservationResponse response = new GetReservationResponse();
         reservationService.findReservationsByAccommodation(accReservationRequest.getAccommodationId())
