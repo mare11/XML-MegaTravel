fac248f24228e39ec424265ea477dd9dac65eef0
mare11 <44523703+mare11@users.noreply.github.com>
2019-06-17 16:55:13 +0200

    add rest calls to auth-service

--- main-server/user-service/src/main/java/org/xmlws/userservice/service/AdminService.java
+++ main-server/user-service/src/main/java/org/xmlws/userservice/service/AdminService.java
@@ -1,10 +1,15 @@
 package org.xmlws.userservice.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.xmlws.dataservice.catalog.CatalogRepository;
+import org.xmlws.userservice.exceptions.UserAlreadyExistsException;
 import org.xmlws.userservice.model.Administrator;
 import org.xmlws.userservice.model.Authority;
+import org.xmlws.userservice.model.AuthorityEnum;
 import org.xmlws.userservice.repository.AdminRepository;
 
 @Service
@@ -16,10 +21,25 @@ public class AdminService {
     @Autowired
     private CatalogRepository catalogRepository;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     public Administrator addAdmin(Administrator administrator) {
-        administrator.setId(catalogRepository.getCatalogId(adminRepository.getRootElementName()));
-        administrator.getAuthority().add(new Authority("ROLE_ADMIN"));
-        return adminRepository.save(administrator);
+
+//        CALL TO AUTH SERVICE FOR UNIQUE USERNAME CHECK
+        ClientResponse clientResponse = webClientBuilder.build()
+                .get()
+                .uri("http://authentication-service/authentication/username/" + administrator.getUsername())
+                .exchange()
+                .block();
+
+        if (clientResponse.statusCode().equals(HttpStatus.OK)) {
+            administrator.setId(catalogRepository.getCatalogId(adminRepository.getRootElementName()));
+            administrator.getAuthority().add(new Authority(AuthorityEnum.ROLE_ADMIN));
+            return adminRepository.save(administrator);
+        }
+
+        throw new UserAlreadyExistsException(administrator.getUsername());
     }
 
     public void deleteAdmin(Administrator administrator) {
--- main-server/user-service/src/main/java/org/xmlws/userservice/service/AgentService.java
+++ main-server/user-service/src/main/java/org/xmlws/userservice/service/AgentService.java
@@ -1,10 +1,15 @@
 package org.xmlws.userservice.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.xmlws.dataservice.catalog.CatalogRepository;
+import org.xmlws.userservice.exceptions.UserAlreadyExistsException;
 import org.xmlws.userservice.model.Agent;
 import org.xmlws.userservice.model.Authority;
+import org.xmlws.userservice.model.AuthorityEnum;
 import org.xmlws.userservice.repository.AgentRepository;
 
 @Service
@@ -16,9 +21,32 @@ public class AgentService {
     @Autowired
     private CatalogRepository catalogRepository;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     public Agent addAgent(Agent agent) {
-        agent.setId(catalogRepository.getCatalogId(agentRepository.getRootElementName()));
-        agent.getAuthority().add(new Authority("ROLE_AGENT"));
-        return agentRepository.save(agent);
+
+//        CALLS TO AUTH SERVICE FOR UNIQUE USERNAME AND EMAIL CHECK
+        ClientResponse usernameClientResponse = webClientBuilder.build()
+                .get()
+                .uri("http://authentication-service/authentication/username/" + agent.getUsername())
+                .exchange()
+                .block();
+
+        if (usernameClientResponse.statusCode().equals(HttpStatus.OK)) {
+            ClientResponse emailClientResponse = webClientBuilder.build()
+                    .get()
+                    .uri("http://authentication-service/authentication/email/" + agent.getEmail())
+                    .exchange()
+                    .block();
+
+            if (emailClientResponse.statusCode().equals(HttpStatus.OK)) {
+                agent.setId(catalogRepository.getCatalogId(agentRepository.getRootElementName()));
+                agent.getAuthority().add(new Authority(AuthorityEnum.ROLE_AGENT));
+                return agentRepository.save(agent);
+            }
+        }
+
+        throw new UserAlreadyExistsException(agent.getUsername(), agent.getEmail());
     }
 }
